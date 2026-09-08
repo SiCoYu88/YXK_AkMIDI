0b1b31ce6f9d730c6e0a34d7b3847e02d6b9671f
SiCoYu88 <wanyouxi@88.com>
2026-09-08 17:36:54 +0800

    新增AkMIDI功能

--- AkPlugins/AkMIDI/Wwise2025.1.9.4397/Source/AudiokineticTools/AudiokineticTools.Build.cs
+++ AkPlugins/AkMIDI/Wwise2025.1.9.4397/Source/AudiokineticTools/AudiokineticTools.Build.cs
@@ -79,6 +79,9 @@ public class AudiokineticTools : ModuleRules
 				"XmlParser",
 
 				"AkAudio",
+#region H3DWwise
+				"AkMIDI",
+#endregion
 				"WwiseAuthoring",
 				"WwiseEditorUtils",
 				"WwiseLowLevelUtils",
---
+++ AkPlugins/AkMIDI/Wwise2025.1.9.4397/Source/AudiokineticTools/Private/AkMidiMessageFactory.h
@@ -0,0 +1,24 @@
+#pragma region H3D
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Factories/Factory.h"
+#include "AkMidiMessageFactory.generated.h"
+
+/**
+ * 
+ */
+UCLASS(hidecategories=Object)
+class AUDIOKINETICTOOLS_API UAkMidiMessageFactory : public UFactory
+{
+	GENERATED_BODY()
+
+public:
+	UAkMidiMessageFactory(const FObjectInitializer& ObjectInitializer);
+
+	virtual UObject* FactoryCreateNew(UClass* Class, UObject* InParent, FName Name, EObjectFlags Flags, UObject* Context, FFeedbackContext* Warn) override;
+};
+
+#pragma endregion
